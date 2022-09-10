b7ed98a9e626ffd7bb6371963bbd88973b5efc38
Ha-min Kim <hakimnz1997@gmail.com>
2022-09-11 11:18:36 +1200

    completed front page of the game

--- BFV/BFV.js
+++ BFV/BFV.js
@@ -1,5 +1,31 @@
 window.addEventListener("load", function () {
-    document.getElementById("button").onclick = function () {
+    document.getElementById("playButton").onclick = function () {
         location.href = "./test.html";
     };
+
+    let gunImageDisplay = document.querySelector("#gunImageDisplay");
+
+    // Displaying weapon images on the front page in random order. 
+    var shuffled_weapons = shuffle(weapons);
+    for (i = 0; i < shuffled_weapons.length; i++) {
+        gunImageDisplay.innerHTML += `<img src="./guns/${weapons[i].name}.png">`;
+    }
+
+    // Shuffling the order of the weapons object array. This way, when the order is shuffled and I'm animating the pictures going from left to right, 
+    // the user will not use that to his advantage and guess the name of the gun, if they find out the pictures are in alphabetical order. 
+    function shuffle(array) {
+        var currentIndex = array.length;
+        var temporaryValue;
+        var randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+        }
 });
