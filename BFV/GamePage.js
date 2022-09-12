8e2d1287ca56e7dbf423b210d7f7a2bf33482f4a
Ha-min Kim <hakimnz1997@gmail.com>
2022-09-12 18:42:15 +1200

    finished adding logic to timer, need to style it

--- BFV/GamePage.js
+++ BFV/GamePage.js
@@ -1,5 +1,63 @@
 window.addEventListener("load", function () {
 
+    // Variables for timer functions
+    let timer = null;
+    let seconds = 60;
+    let minutes = "0" + 9;
+ 
+    // Start countdown of 10 minutes when the start button is clicked. 
+    document.querySelector("#btnStartTimer").addEventListener("click", startTimer);
+   // document.querySelector("#btnStopTimer").addEventListener("click", stopTimer);
+
+   // Function executes when start button is clicked. 
+    function startTimer(){
+        if (timer == null) {
+            document.querySelector("#timer_face").innerHTML = "10:00"; // removed timer = from the start of the line.
+            
+
+            timer = setInterval(updateSeconds, 1000); // Every seconds, the timer drops by 1 second. 
+            timer = setInterval(updateMinutes, 60000); // Every minutes, the timer drops by 1 minute. 
+        }
+    }
+
+    // setInterval every second within the startTimer function
+    function updateSeconds() {
+        seconds --;
+        
+        // if seconds or minutes have 1 significant figure, add a 0 at the beginning. so 1:1 would be 01:01.
+        if (seconds < 10) {
+            document.querySelector("#timer_face").innerHTML = minutes + ":" + "0" + seconds;
+        }
+        else {
+            document.querySelector("#timer_face").innerHTML = minutes + ":" + seconds;
+        }
+        
+        if (seconds == 0) {
+            seconds = 60;
+        }
+    }
+
+    // setInterval every minute within the startTimer function
+    function updateMinutes() {
+        minutes --;
+    }
+
+
+    stopGame();
+
+    function stopGame() {
+        if (minutes == 0 && seconds == 0) {
+            console.log(gameover);
+            clearInterval(timer);
+        }
+    }
+
+
+
+
+
+
+
 
     let weaponImage = document.querySelector("#weaponImage");
 
@@ -27,9 +85,7 @@ window.addEventListener("load", function () {
         return array;
     }
 
-    function timer(){
-        
-    }
+
 
     // This function should randomly display an image and prompt the user to guess. 
     // Once a weapon is displayed, it shouldn't be displayed in future random cycles. 
