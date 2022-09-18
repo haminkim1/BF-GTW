b19c1f973e07b7358f81d0ddcbc626e8fb488c12
Ha-min Kim <hakimnz1997@gmail.com>
2022-09-18 23:10:52 +1200

    tried attempting to correct autocompletetext logic

--- BFV/GamePage.js
+++ BFV/GamePage.js
@@ -1,100 +1,75 @@
 window.addEventListener("load", function () {
 
-    // Variables for timer functions
-    let timer = null;
-    let timerMinutes = null;
-    let seconds = 60;
-    let minutes = 9;
- 
-    // Start countdown of 10 minutes when the start button is clicked. 
-    document.querySelector("#btnStartTimer").addEventListener("click", startTimer);
-    document.querySelector("#btnStopTimer").addEventListener("click", stopTimer);
-
-    // Function executes when start button is clicked. 
-    function startTimer(){
-        if (timer == null && timerMinutes == null) {
-            document.querySelector("#timer_face").innerHTML = "10:00"; // removed timer = from the start of the line.
-            
-            timer = setInterval(updateSeconds, 1000); // Every second, the timer drops by 1 second. 
-            timerMinutes = setInterval(updateMinutes, 60000); // Every minute, the timer drops by 1 minute. 
-        }
-    }
-
-    // setInterval every second within the startTimer function
-    function updateSeconds() {
-        seconds --;
-        
-        // if seconds or minutes have 1 significant figure, add a 0 at the beginning. so 1:1 would be 01:01.
-        if (seconds < 10) {
-            document.querySelector("#timer_face").innerHTML = "0" + minutes + ":" + "0" + seconds;
-        }
-        else {
-            document.querySelector("#timer_face").innerHTML = "0" + minutes + ":" + seconds;
-        }
-        
-        if (minutes != 0 && seconds == 0) {
-            seconds = 60;
-        }
-
-        stopSecondsTimer();
-    }
-
-    // setInterval every minute within the startTimer function
-    function updateMinutes() {
-        minutes --;
-        stopMinutesTimer();
-    }
-
-    function stopTimer() {
-        clearInterval(timer);
-        clearInterval(timerMinutes);
-        minutes = 0;
-        seconds = 0;
-        document.querySelector("#timer_face").innerHTML = "0" + minutes + ":" + "0" + seconds;
-    }
-
-    // Above are the timer related codes
-    // Below are the game related codes
-
     document.querySelector("#submit").addEventListener("click", submitButton);
     let score = 0;
 
     // Press the submit button by pressing the "Enter" button
     const inputWeaponText = document.getElementById("weaponText");
 
-    inputWeaponText.addEventListener("keypress", function(event) {
+    inputWeaponText.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
             document.getElementById("submit").click();
         }
     });
 
+    // Require an unshuffled order of the weapons as a new array.
+    var unshuffledWeapons = [...weapons];
     // Displaying weapon images on the front page in random order. 
     var shuffled_weapons = shuffle(weapons);
 
 
+
+    // This event listener will search if the first letters of the weapons names match with the letters of the input typed. 
+    // Get value of input
+    // Convert value of input into array
+    // Compare first index of input array vs weapons name array. (Need to think about which loop to use)
+    // If first index match, print "match".
+    // If second and etc match, print "match". 
+    // Next would be to somehow add all the matching weapon names below the weapon input. Once that name is clicked, it'll copy and paste to the input area. 
+    // Once the name is clicked, it will hide the auto complete name div again. 
+    inputWeaponText.addEventListener("input", function () {
+        const weaponAutoCompleteList = document.querySelector("#weaponAutoCompleteList");
+        const valueOfInput = document.getElementById("weaponText").value;
+        const arrayOfInput = Array.of(...valueOfInput)
+
+
+        console.log(valueOfInput)
+        if (arrayOfInput.length == 0) {
+            weaponAutoCompleteList.innerHTML = "";
+        }
+        else {
+            for (let i in unshuffledWeapons) {
+                if (valueOfInput.toUpperCase() == unshuffledWeapons[i].name.charAt(arrayOfInput.length - 1).toUpperCase()) {
+                    weaponAutoCompleteList.innerHTML += `
+                    <div class="hiddenLists">
+                        <span><strong id="hiddenWords">${unshuffledWeapons[i].name}</strong></span>
+                    </div>`;
+
+                }
+     
+            }
+        }
+        const x = Object.values(unshuffledWeapons);
+
+
+    })
+
+
+
+
+
     displayGame();
-    
+
+
+
+    // Loading a random weapon image. 
     function displayGame() {
-        let weaponImage = document.querySelector("#weaponImage");  
+        let weaponImage = document.querySelector("#weaponImage");
         weaponImage.innerHTML = `<img src="./guns/${shuffled_weapons[0].name}.png" id="weaponImageDisplay">`;
         document.getElementById("weaponText").value = "";
     }
 
-
-    function stopSecondsTimer() {
-        if (minutes == 0 && seconds == 0) {
-            clearInterval(timer);
-            console.log("seconds stopped");
-        }
-    }
-
-    function stopMinutesTimer() {
-        if (minutes == 0) {
-            clearInterval(timerMinutes);
-            console.log("Game Over");
-        }
-    }
 
     // Shuffling the order of the weapons object array. This way, when the order is shuffled and I'm animating the pictures going from left to right, 
     // the user will not use that to his advantage and guess the name of the gun, if they find out the pictures are in alphabetical order. 
@@ -124,7 +99,7 @@ window.addEventListener("load", function () {
             console.log("correct");
             shuffled_weapons.shift();
             console.log(shuffled_weapons);
-            tallyScore(score+1);
+            tallyScore(score + 1);
             score++;
             displayGame();
         }
@@ -135,7 +110,7 @@ window.addEventListener("load", function () {
         }
 
         stopGameAfterNoHealth(health);
-        
+
     }
 
     function tallyScore(score) {
