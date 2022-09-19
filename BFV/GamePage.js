d557bfbe7998cef5ddae0cef5e3d6d6734e2020f
Ha-min Kim <hakimnz1997@gmail.com>
2022-09-19 23:35:37 +1200

    attempting closing autocomplete list after clicking on it function

--- BFV/GamePage.js
+++ BFV/GamePage.js
@@ -1,10 +1,11 @@
 window.addEventListener("load", function () {
 
     document.querySelector("#submit").addEventListener("click", submitButton);
-    let score = 0;
+
 
     // Press the submit button by pressing the "Enter" button
     const inputWeaponText = document.getElementById("weaponText");
+  
 
     inputWeaponText.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
@@ -18,77 +19,12 @@ window.addEventListener("load", function () {
     // Displaying weapon images on the front page in random order. 
     var shuffled_weapons = shuffle(weapons);
 
+    autocompleteList();
 
-
-    // This event listener will search if the first letters of the weapons names match with the letters of the input typed. 
-    // Get value of input
-    // Convert value of input into array
-    // Compare first index of input array vs weapons name array. (Need to think about which loop to use)
-    // If first index match, print "match".
-    // If second and etc match, print "match". 
-    // Next would be to somehow add all the matching weapon names below the weapon input. Once that name is clicked, it'll copy and paste to the input area. 
-    // Once the name is clicked, it will hide the auto complete name div again. 
-    inputWeaponText.addEventListener("input", function () {
-        const weaponAutoCompleteList = document.querySelector("#weaponAutoCompleteList");
-        const valueOfInput = document.getElementById("weaponText").value;
-        const arrayOfInput = Array.of(...valueOfInput)
-
-        const y = Object.values(unshuffledWeapons);
-        const x = unshuffledWeapons.every((unshuffledWeapons) => {
-            return unshuffledWeapons.name.charAt(arrayOfInput.length - 1).toUpperCase() == valueOfInput.toUpperCase()
-        });
-        console.log (x)
-        https://www.youtube.com/playlist?list=PLDlWc9AfQBfZGZXFb_1tcRKwtCavR7AfT
-        /*
-        
-        if input triggered, clear newWeaponsList array
-            if valueOfInput == unshuffledWeapons.name.charAt
-            create array (newWeaponsList) of all objects matching valueOfInput
-            display autocomplete list of that new array
-            
-            else if input triggered, if valueOfInput != unshuffledWeapons.name.charAt
-            clear autocomplete list
-                I prob don't need this else if statement, if input triggered and only IF valueOfInput == unshuffledWeapons.name.charAt
-                then a new array will be created. However, if only input is triggered and the IF statement is false, it'll clear the new array, hence
-                will not display the autocomplete list
-                
-
-
-        */
-
-
-        console.log(valueOfInput)
-        if (arrayOfInput.length == 0) {
-            weaponAutoCompleteList.innerHTML = "";
-        }
-        else {
-            for (let i in unshuffledWeapons) {
-                if (valueOfInput.toUpperCase() == unshuffledWeapons[i].name.charAt(arrayOfInput.length - 1).toUpperCase() && x === false) {
-                    weaponAutoCompleteList.innerHTML += `
-                    <div class="hiddenLists">
-                        <span><strong id="hiddenWords">${unshuffledWeapons[i].name}</strong></span>
-                    </div>`;
-
-                }
-            }
-        }
- 
-    })
-
-
-
-
+    
 
     displayGame();
-
-
-
-    // Loading a random weapon image. 
-    function displayGame() {
-        let weaponImage = document.querySelector("#weaponImage");
-        weaponImage.innerHTML = `<img src="./guns/${shuffled_weapons[0].name}.png" id="weaponImageDisplay">`;
-        document.getElementById("weaponText").value = "";
-    }
+    let score = 0;
 
 
     // Shuffling the order of the weapons object array. This way, when the order is shuffled and I'm animating the pictures going from left to right, 
@@ -108,6 +44,77 @@ window.addEventListener("load", function () {
 
         return array;
     }
+
+
+    function autocompleteList() {
+        // When input box is typed, it'll have autocomplete list of weapons starting with the letters typed in the input box. 
+        inputWeaponText.addEventListener("input", function () {
+            const weaponAutoCompleteList = document.querySelector("#weaponAutoCompleteList");
+            const valueOfInput = document.getElementById("weaponText").value;
+            const arrayOfInput = Array.of(...valueOfInput)
+
+            let newWeaponsList = []; // This array will be used later on. Weapons that have matching name with the input will be pushed and copied into this array. 
+            /*    
+            if input triggered, clear newWeaponsList array
+                if valueOfInput == unshuffledWeapons.name.charAt
+                create array (newWeaponsList) of all objects matching valueOfInput
+                display autocomplete list of that new array
+                
+                else if input triggered, if valueOfInput != unshuffledWeapons.name.charAt
+                clear autocomplete list
+                    I prob don't need this else if statement, if input triggered and only IF valueOfInput == unshuffledWeapons.name.charAt
+                    then a new array will be created. However, if only input is triggered and the IF statement is false, it'll clear the new array, hence
+                    will not display the autocomplete list
+            */
+            // If the input box is empty, clear all autocomplete list
+            if (arrayOfInput.length == 0) {
+                weaponAutoCompleteList.innerHTML = "";
+            }
+            else {
+                // Cycles through the UNSHUFFLED array which contains the names of all the weapons. If the starting letters of the weapon match with the input letters,
+                // those weapons will be copied into a new array called newWeaponsList. This array will be used to display the autocomplete list. 
+                for (let i in unshuffledWeapons) {
+                    if (valueOfInput.toUpperCase() == unshuffledWeapons[i].name.substring(0, arrayOfInput.length).toUpperCase()) {
+                        newWeaponsList.push(unshuffledWeapons[i].name)
+                    }
+                    else if (valueOfInput.toUpperCase() != unshuffledWeapons[i].name.substring(0, arrayOfInput.length).toUpperCase()) {
+                        weaponAutoCompleteList.innerHTML = "";
+                    }
+                }
+                // Lists all the weapons on the autocomplete list with the array that contains lists of all the weapons that start with the letters on the input box. 
+                for (j = 0; j < newWeaponsList.length; j++) {
+                    weaponAutoCompleteList.innerHTML += `
+                <div class="hiddenLists">
+                    <span id="hiddenWords"><strong>${newWeaponsList[j]}</strong></span>
+                </div>`;
+                    clickAutocompleteList(newWeaponsList[j]);
+                }
+            }
+        })
+    }
+
+
+
+
+    function clickAutocompleteList(weapon) {
+        document.querySelector("#weaponAutoCompleteList").addEventListener("click", function(){
+            document.getElementById("weaponText").value.innerHTML = weapon;
+            console.log("Works");
+            console.log(document.getElementById("weaponText").value.innerText);
+            console.log(weapon);
+        })
+
+    }
+
+    // Loading a random weapon image. 
+    function displayGame() {
+        let weaponImage = document.querySelector("#weaponImage");
+        weaponImage.innerHTML = `<img src="./guns/${shuffled_weapons[0].name}.png" id="weaponImageDisplay">`;
+        document.getElementById("weaponText").value = "";
+    }
+
+
+
 
     // This function should randomly display an image and prompt the user to guess. 
     // Once a weapon is displayed, it shouldn't be displayed in future random cycles. 
