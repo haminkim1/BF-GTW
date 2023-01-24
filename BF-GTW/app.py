174dfc7e5182b52c26ad7b67175ea8115225641a
Ha-min Kim <hakimnz1997@gmail.com>
2023-01-24 18:35:52 +1300

    Added bits to index.html, optionally need to style more

--- BF-GTW/app.py
+++ BF-GTW/app.py
@@ -1,4 +1,10 @@
 from app import app
+from flask_session import Session
 
 if __name__ == "__main__":
     app.run()
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
