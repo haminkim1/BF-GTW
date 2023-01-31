23b1c34c280628170e2d9ec92ee8ad520e08f3a9
Ha-min Kim <hakimnz1997@gmail.com>
2023-01-31 21:28:31 +1300

    finally fixed adding shadow and background image on bfv, need to repeat the same with the other Battlefield games

--- BF-GTW/app/static/js/bfv.js
+++ BF-GTW/app/static/js/bfv.js
@@ -2,14 +2,15 @@ window.addEventListener("load", function () {
 
     // changing class within body tag in layout.html whenever bfv.html loads. 
     // the function loads devastation.webp instead of having a black background. 
-    // const background = document.querySelector(".background");
-    // background.classList.remove("background");
-    // const main = document.querySelector("main");
-    // main.className = '';
-    // // background.classList.add("bfv-main")
-    // // const main = document.querySelector("main")
-    // main.classList.add("bfv-main")
-    // main.classList.add("shadow")
+    const background = document.querySelector(".background");
+    background.classList.remove("background");
+    
+    const parentDiv = document.querySelector(".parentDiv");
+    parentDiv.classList.add("bfv-main")
+
+    const changeToShadowClass = document.querySelector(".changeToShadowClass");
+    changeToShadowClass.className = '';
+    changeToShadowClass.classList.add("shadow")
 })
 
 
