9ecd735a869e735937649d6cb0f81d58f072ca11
Ha-min Kim <hakimnz1997@gmail.com>
2023-02-16 18:06:42 +1300

    Able to print high score table on client side

--- BF-GTW/app/profile.py
+++ BF-GTW/app/profile.py
@@ -16,111 +16,6 @@ def view_profile():
     # Selects logged in user's completed games
     games = db.execute("SELECT * FROM game_log WHERE user_id = ? ORDER BY game_date DESC", session["user_id"])
 
-    # Selects logged in user's highest score from easy, medium, hard mode for BF4, BF1, BFV, BF2042
-    # Insert score/total_weapons on top, DATE(game_date) on bottom as a split cell. 
-    """
-            easy|medium|high
-    BF4     S/T_W
-            date
-    BF1     S/T_W
-            date
-    BFV     S/T_W
-            date
-    BF2042  S/T_W
-            date
-    """
-    
-    bf4HighScores = db.execute("""SELECT UPPER(BF_game), mode, MAX(score) AS highest_score, total_weapons, DATE(game_date) 
-    FROM game_log 
-    WHERE user_id = ? AND BF_game IN ('bf4') AND mode IN ('easy', 'medium', 'hard') 
-    GROUP BY BF_game, mode 
-    ORDER BY 
-        CASE 
-            WHEN mode = 'easy' THEN 1 
-            WHEN mode = 'medium' THEN 2 
-            WHEN mode = 'hard' THEN 3 
-        END""", session["user_id"])
-    
-    bf1HighScores = db.execute("""SELECT UPPER(BF_game), mode, MAX(score) AS highest_score, total_weapons, DATE(game_date) 
-    FROM game_log 
-    WHERE user_id = ? AND BF_game IN ('bf1') AND mode IN ('easy', 'medium', 'hard') 
-    GROUP BY BF_game, mode 
-    ORDER BY 
-        CASE 
-            WHEN mode = 'easy' THEN 1 
-            WHEN mode = 'medium' THEN 2 
-            WHEN mode = 'hard' THEN 3 
-        END""", session["user_id"])
-    
-    bfvHighScores = db.execute("""SELECT UPPER(BF_game), mode, MAX(score) AS highest_score, total_weapons, DATE(game_date) 
-    FROM game_log 
-    WHERE user_id = ? AND BF_game IN ('bfv') AND mode IN ('easy', 'medium', 'hard') 
-    GROUP BY BF_game, mode 
-    ORDER BY 
-        CASE 
-            WHEN mode = 'easy' THEN 1 
-            WHEN mode = 'medium' THEN 2 
-            WHEN mode = 'hard' THEN 3 
-        END""", session["user_id"])
-
-    bf2042HighScores = db.execute("""SELECT UPPER(BF_game), mode, MAX(score) AS highest_score, total_weapons, DATE(game_date) 
-    FROM game_log 
-    WHERE user_id = ? AND BF_game IN ('bf2042') AND mode IN ('easy', 'medium', 'hard') 
-    GROUP BY BF_game, mode 
-    ORDER BY 
-        CASE 
-            WHEN mode = 'easy' THEN 1 
-            WHEN mode = 'medium' THEN 2 
-            WHEN mode = 'hard' THEN 3 
-        END""", session["user_id"])
-    
-    # highScores = db.execute("""SELECT UPPER(BF_game), mode, MAX(score) AS highest_score, total_weapons, DATE(game_date) 
-    # FROM game_log 
-    # WHERE user_id = ? AND BF_game IN ('bf4', 'bf1', 'bfv', 'bf2042') AND mode IN ('easy', 'medium', 'hard') 
-    # GROUP BY BF_game, mode 
-    # ORDER BY 
-    #     CASE 
-    #         WHEN BF_game = 'bf4' THEN 1 
-    #         WHEN BF_game = 'bf1' THEN 2 
-    #         WHEN BF_game = 'bfv' THEN 3 
-    #         WHEN BF_game = 'bf2042' THEN 4 
-    #     END, 
-    #     CASE 
-    #         WHEN mode = 'easy' THEN 1 
-    #         WHEN mode = 'medium' THEN 2 
-    #         WHEN mode = 'hard' THEN 3 
-    #     END""", session["user_id"])
-    
-    # print(highScores)
-    print(bfvHighScores)
-
-    # get list of BF_games from table
-    # get list of modes from table
-    # Use a loop for each combination of BF_games and modes
-    # Create empty highScores list
-    # Starting at BF4, BF1, BFV, BF2042:
-        # if BF_games & modes have score, leave as is
-        # else somehow put an empty data 
-        # Append to highScores list
-        # Repeat for other combinations
-    # Send highScores list to client
-    highScores = []
-    BF_games = ["bf4", "bf1", "bfv", "bf2042"]
-    modes = ["easy", "medium", "hard"]
-
-    
-    for BF_game in BF_games:
-        for mode in modes:
-            score = db.execute("""SELECT MAX(score) AS highest_score FROM game_log WHERE user_id = ? AND BF_game = ? AND mode = ?"""
-                   , session["user_id"], BF_game, mode)  
-            print(score)
-            if not score:
-                score[0] = None
-            highScores.append(score[0])
-    print(highScores)
-    
-
-
     return render_template("admin/profile.html", usersData=usersData, games=games)
 
 @app.route("/profile/highscore")
@@ -128,40 +23,30 @@ def view_profile():
 def load_highscore():
     BF_games = ["bf4", "bf1", "bfv", "bf2042"]
     modes = ["easy", "medium", "hard"]
-
-    # To automate the process, each variable name can be such as one, two or three instead of bf4highscores. 
-    # The BF_game value that'll be passed to client will be the HEADING for each row. 
-    bf4HighScores = {
-        "BF_game": "BF4",
-        "mode":[
-            {"easy": 1,
-            "medium": 2,
-            "hard": 3
-            }
-        ]
-    }
-    print(bf4HighScores["mode"][0]["easy"])
-    bf1HighScores = {
-        "BF_game": "BF1",
-        "easy": 4,
-        "medium": 5,
-        "hard": 6
-    }
-
     highScores = []
 
+    # highScore data structure = {
+    #     "BF_game": "BF_game (eg BF4)",
+    #     "mode": {
+    #         "easy": highest_score,
+    #         "medium": highest_score,
+    #         "hard": highest_score
+    #         }
+        
+    # }
     for BF_game in BF_games:
         scores = {
             "BF_game": BF_game,
-            "modes":modes
+            "modes":{}
         }
+        for mode in modes:
+            score = db.execute("""SELECT MAX(score) AS highest_score 
+                FROM game_log 
+                WHERE user_id = ? AND BF_game = ? AND mode = ?"""
+                , session["user_id"], BF_game, mode)  
+            
+            scores["modes"][mode] = score[0]["highest_score"]
 
-        for index, mode in enumerate(modes):
-            score = db.execute("""SELECT MAX(score) AS highest_score FROM game_log WHERE user_id = ? AND BF_game = ? AND mode = ?"""
-                   , session["user_id"], BF_game, mode)  
-            scores["modes"][index][mode] = score[0]["highest_score"]
-
-        highScores.append(scores)
-    
+        highScores.append(scores)   
 
     return jsonify(highScores)
--- BF-GTW/app/static/js/profile.js
+++ BF-GTW/app/static/js/profile.js
@@ -4,44 +4,45 @@ window.addEventListener("load", function () {
 
 
     async function getHighScoreData() {
-        let table = document.getElementById("high-score-table");
         try {
             const response = await fetch("/profile/highscore");
             let highScores = await response.json();
 
-            table.innerHTML = "";
-            table.innerHTML = `
-            <tr>
-                <th></th>
-                <th>Easy</th>
-                <th>Medium</th>
-                <th>Hard</th>
-            </th>
-            `
-            for (let i in highScores) {
-                table.innerHTML += `
-                <tr>
-                    <th>${highScores[i].BF_game}</th>
-                    <td class="high-score-cells">${highScores[i].easy}</th>
-                    <td class="high-score-cells">${highScores[i].medium}</th>
-                    <td class="high-score-cells">${highScores[i].hard}</th>
-                </tr>
-                `
-            };
-
-            // let highScoreCells = document.querySelectorAll(".high-score-cells");
-            // console.log(highScoreCells[0].textContent);
-            
-            // for (let i in highScoreCells) {
-            //     if (highScoreCells[i].textContent === null) {
-            //         highScoreCells[i].innerHTML = `<td class="high-score-cells">1</td>`;
-            //     }
-            // }
-
- 
+            populateHighScoreTable(highScores);
         } catch (error) {
             console.error(error);
         }
+    }
+
+    function populateHighScoreTable (data) {
+        let table = document.getElementById("high-score-table");
+        table.innerHTML = `
+        <tr>
+            <th></th>
+            <th>Easy</th>
+            <th>Medium</th>
+            <th>Hard</th>
+        </th>
+        `
+        for (let i in data) {
+            table.innerHTML += `
+            <tr>
+                <th id="BF-game-heading">${data[i].BF_game}</th>
+                <td id="${data[i].BF_game}-easy"></td>
+                <td id="${data[i].BF_game}-medium"></td>
+                <td id="${data[i].BF_game}-hard"></td>
+            </tr>
+            `
+            if (data[i].modes.easy !== null) {
+                document.getElementById(`${data[i].BF_game}-easy`).innerText = `${data[i].modes.easy}`;
+            }
+            if (data[i].modes.medium !== null) {
+                document.getElementById(`${data[i].BF_game}-medium`).innerText = `${data[i].modes.medium}`;
+            }
+            if (data[i].modes.hard !== null) {
+                document.getElementById(`${data[i].BF_game}-hard`).innerText = `${data[i].modes.hard}`;
+            }
+        };
     }
 })
 
