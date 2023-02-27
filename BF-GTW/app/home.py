406dcdaf6dce7d176989585f9d26b40cde4bf1da
Ha-min Kim <hakimnz1997@gmail.com>
2023-02-27 20:36:15 +1300

    Completed game_over.html

--- BF-GTW/app/home.py
+++ BF-GTW/app/home.py
@@ -1,11 +1,17 @@
-from flask import render_template
+from flask import render_template, session
 
 from app import app
+from app.db.db_config import db
 from modules.apology import apology
 
 @app.route("/")
 def home():
-    return render_template("public/index.html")
+    if session["user_id"]:
+        rows = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+        username = rows[0]["username"]
+        return render_template("public/index.html", username=username)
+    else:
+        return render_template("public/index.html")
 
 @app.route('/', defaults={'path': ''}, methods=['GET'])
 @app.route('/<path:path>', methods=['GET'])
